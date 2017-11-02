bed235cbe0b777c66badb7047c77497f2f02312a
rebeccanewborn <rjn58@cornell.edu>
2017-11-02 09:28:36 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,47 @@
+class Driver {
+  constructor(name, startDate) {
+    this.name = name;
+    this.startDate = new Date(startDate);
+  }
 
+  yearsExperienceFromBeginningOf(endDate) {
+    return endDate - 1 - this.startDate.getUTCFullYear();
+  }
+}
+
+class Route {
+  constructor(beginningLocation, endingLocation) {
+    this.beginningLocation = beginningLocation;
+    this.endingLocation = endingLocation;
+  }
+
+  blocksTravelled() {
+    console.log(this.beginningLocation);
+    console.log(this.endingLocation);
+    let horizDist = Math.abs(
+      eastWest.indexOf(this.beginningLocation.horizontal) -
+        eastWest.indexOf(this.endingLocation.horizontal)
+    );
+    let vertDist = Math.abs(
+      this.beginningLocation.vertical - this.endingLocation.vertical
+    );
+    return horizDist + vertDist;
+  }
+
+  estimatedTime(peak) {
+    if (peak) {
+      return this.blocksTravelled() / 2;
+    }
+    return this.blocksTravelled() / 3;
+  }
+}
+
+let eastWest = [
+  "1st Avenue",
+  "2nd Avenue",
+  "3rd Avenue",
+  "Lexington Avenue",
+  "Park",
+  "Madison Avenue",
+  "5th Avenue"
+];
